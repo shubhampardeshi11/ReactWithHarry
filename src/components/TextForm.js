eeb6700145ed6a2ea82d6b436cd909833500f651
shubhampardeshi11 <shubham.pardeshi@shadesofweb.com>
2023-08-14 13:14:00 +0530

    Adding more logic to TextUtils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,22 +8,36 @@ export default function TextForm(props) {
         setText(newText);
     }
 
+    const handleLowClick = () => {
+        console.log("Lowercase was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
     const handleOnChange = (event) => {
         console.log("On Change");
         setText(event.target.value);
     }
-    const [text, setText] = useState("Enter Text here");
+    const [text, setText] = useState('');
 
     // setText("New Text"); Correct way to change the state
 
     return (
-        <div>
-            <div className="my-3">
-
+        <>
+            <div className='container my-3'>
                 <h1>{props.heading}</h1>
                 <textarea className="form-control" value={text} id="textBox" rows="7" onChange={handleOnChange}></textarea>
                 <button className="btn btn-primary my-2" onClick={handleUpClick}>Convert To Uppercase</button>
+                <button className="btn btn-primary mx-2 my-2" onClick={handleLowClick}>Convert To Lowercase</button>
             </div>
-        </div>
+
+
+            <div className='container my-2'>
+                <h3>Your Text Summary</h3>
+                <p> {text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.08 * text.split(" ").length} read time</p>
+                <h3>Preview</h3>
+                <p>{text}</p>
+            </div>
+        </>
     )
 }
